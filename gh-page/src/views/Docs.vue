45f8b2c19d2c236f9223bebf5cd785d661066fa2
lopo <lopoflyfly@gmail.com>
2022-02-22 11:25:47 +0800

    feat: docs page

---
+++ gh-page/src/views/Docs.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="docs">
+        <div class="docs-nav-bar">
+            nav
+        </div>
+        <div class="docs-body">
+            <div class="docs-side-menu">
+                menu
+            </div>
+            <div class="docs-item-docs">
+                <router-view />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Docs"
+}
+</script>
+
+<style scoped>
+
+</style>
