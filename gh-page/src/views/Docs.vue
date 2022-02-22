505f65bf481fdaba0c984f21b9c2e0101f987575
lopo <lopoflyfly@gmail.com>
2022-02-22 18:14:39 +0800

    feat: menu structure

--- gh-page/src/views/Docs.vue
+++ gh-page/src/views/Docs.vue
@@ -1,25 +1,56 @@
 <template>
     <div class="docs">
-        <div class="docs-nav-bar">
-            nav
+        <div class="docs-side-menu">
+            <el-menu>
+                <el-sub-menu index="clone" title="clone">
+                    <el-menu-item index="deep">deep</el-menu-item>
+                    <el-menu-item index="plain">plain</el-menu-item>
+                </el-sub-menu>
+                <el-sub-menu index="queue" title="queue">
+                    <el-menu-item index="123">222</el-menu-item>
+                </el-sub-menu>
+            </el-menu>
         </div>
-        <div class="docs-body">
-            <div class="docs-side-menu">
-                menu
-            </div>
-            <div class="docs-item-docs">
-                <router-view />
-            </div>
+        <div class="docs-item-docs">
+            <router-view />
         </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: "Docs"
-}
+<script lang="ts">
+import {defineComponent} from "vue";
+import {ElMenu, ElSubMenu, ElMenuItem} from "element-plus";
+
+export default defineComponent({
+    name: "Docs",
+    components: {
+        ElMenu, ElSubMenu, ElMenuItem
+    },
+    setup() {
+
+    }
+})
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.docs {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
+    .docs-side-menu {
+        position: relative;
+        width: 259px;
+        height: 100%;
+        border-right: solid 1px #dcdfe6;
+    }
+    .docs-item-docs {
+        position: relative;
+        width: calc(100% - 260px);
+        height: 100%;
+    }
+}
 </style>
