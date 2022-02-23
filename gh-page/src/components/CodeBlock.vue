361d51b7bca3841c6db176998503d6d52bd5e826
lopo <lopoflyfly@gmail.com>
2022-02-23 17:31:54 +0800

    feat: cold-block component

---
+++ gh-page/src/components/CodeBlock.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="code-block">
+
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted, onUnmounted, shallowRef} from "vue";
+import {EditorView} from "@codemirror/basic-setup";
+
+export default defineComponent({
+    name: "CodeBlock",
+    setup() {
+        const codeBlock = shallowRef<EditorView | null>(null)
+
+        onMounted(() => {
+
+        })
+        onUnmounted(() => {
+
+        })
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.code-block {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
