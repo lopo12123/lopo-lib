ca67974237af2616d92a2ea7dc83a0e8f6bd41f1
ChenMH <2036604698@qq.com>
2022-02-26 16:41:11 +0800

    config: build configs

--- gh-page/vite.config.ts
+++ gh-page/vite.config.ts
@@ -5,6 +5,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [vue(), vueJsx()],
     resolve: {
         alias: {
@@ -13,5 +14,9 @@ export default defineConfig({
     },
     server: {
         port: 8081
+    },
+    build: {
+        outDir: '../docs',
+        emptyOutDir: true
     }
 })
