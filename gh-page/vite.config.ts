74f8545f8517d73cf72a24bfdca24ebf7f4003ac
ChenMH <2036604698@qq.com>
2022-02-26 21:54:25 +0800

    docs: v2.0.1-chunked

--- gh-page/vite.config.ts
+++ gh-page/vite.config.ts
@@ -17,6 +17,15 @@ export default defineConfig({
     },
     build: {
         outDir: '../docs',
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if(id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString()
+                    }
+                }
+            }
+        }
     }
 })
