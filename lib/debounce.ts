76b09f45df17d429697bd806cddf34d0c1fb954f
lopo <lopoflyfly@gmail.com>
2022-05-05 17:27:53 +0800

    fix: 防抖

--- lib/debounce.ts
+++ lib/debounce.ts
@@ -1,35 +1,33 @@
 /**
  * [Example & Test]{@link https://github.com/lopo12123/lopo-lib/blob/master/test/test-debounce.ts}
- * @description [防抖] min_trigger(ms)时间间隔内最多执行一次fn
+ * @description [防抖] 触发后静置时长达到 min_hold 后执行, 否则认为是抖动并忽略
  * @param fn 触发函数
- * @param min_trigger 最小触发时间间隔 (单位: ms)
+ * @param min_hold 最小维持时间 (单位: ms)
  * @example
  * import debounce from "lopo-lib/lib/debounce";
  *
  * // 使用debounce包裹目标函数
- * const logger = debounce((loop: number) => {
- *     console.log('function in debounce, now in loop ', loop)
+ * const logger = debounce((trigger: number) => {
+ *    console.log('function in debounce, now in trigger ', trigger)
  * }, 1000)
  *
- * let loop_count = 0
- * // 使用interval模拟频繁触发的事件
+ * let trigger_count = 0
+ *
+ * // 使用interval模拟频繁抖动
  * const timer = setInterval(() => {
- *     console.log('now in loop ', loop_count)
- *     logger(loop_count)
- *     if(loop_count ++ > 4) clearInterval(timer)
+ *     console.log('now in trigger ', trigger_count)
+ *     logger(trigger_count)
+ *     if(trigger_count++ > 4) clearInterval(timer)
  * }, 500)
  */
-const debounce = <Args extends Array<any>>(fn: (...args: Args) => void, min_trigger: number) => {
-    let block_timer: any = null
+const debounce = <Args extends Array<any>>(fn: (...args: Args) => void, min_hold: number) => {
+    let debounce_timer: any = null
     return (...args: Args) => {
-        if(block_timer === null) {
-            fn(...args)
+        if(debounce_timer !== null) clearInterval(debounce_timer)
 
-            block_timer = setTimeout(() => {
-                block_timer = null
-                clearTimeout(block_timer)
-            }, min_trigger)
-        }
+        debounce_timer = setTimeout(() => {
+            fn(...args)
+        }, min_hold)
     }
 }
 
--- test/test-debounce.ts
+++ test/test-debounce.ts
@@ -4,29 +4,27 @@ import debounce from "../lib/debounce";
  * @example
  * // 运行`ts-node test-debounce.ts`
  * // 输出:
- * now in loop  0
- * function in debounce, now in loop  0
- * now in loop  1
- * now in loop  2
- * function in debounce, now in loop  2
- * now in loop  3
- * now in loop  4
- * function in debounce, now in loop  4
- * now in loop  5
+ * now in trigger  0
+ * now in trigger  1
+ * now in trigger  2
+ * now in trigger  3
+ * now in trigger  4
+ * now in trigger  5
+ * function in debounce, now in trigger  5
  */
 const do_test = () => {
     // 使用debounce包裹目标函数
-    const logger = debounce((loop: number) => {
-        console.log('function in debounce, now in loop ', loop)
+    const logger = debounce((trigger: number) => {
+        console.log('function in debounce, now in trigger ', trigger)
     }, 1000)
 
-    let loop_count = 0
+    let trigger_count = 0
 
-    // 使用interval模拟频繁触发的事件
+    // 使用interval模拟频繁抖动
     const timer = setInterval(() => {
-        console.log('now in loop ', loop_count)
-        logger(loop_count)
-        if(loop_count++ > 4) clearInterval(timer)
+        console.log('now in trigger ', trigger_count)
+        logger(trigger_count)
+        if(trigger_count++ > 4) clearInterval(timer)
     }, 500)
 }
 
