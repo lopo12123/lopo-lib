3745ca73acdf29a91b53c69d38406dcef5b48f01
lopo <lopoflyfly@gmail.com>
2022-05-05 16:59:22 +0800

    feat: 防抖

---
+++ lib/debounce.ts
@@ -0,0 +1,48 @@
+/**
+ * @description [防抖] min_trigger(ms)时间间隔内最多执行一次fn
+ * @param fn 触发函数
+ * @param min_trigger 最小触发时间间隔 (单位: ms)
+ * @example
+ * /// location: "../test/test-debounce.ts"
+ * import debounce from "lopo-lib/lib/debounce";
+ *
+ * // 使用debounce包裹目标函数
+ * const logger = debounce((loop: number) => {
+ *     console.log('function in debounce, now in loop ', loop)
+ * }, 1000)
+ *
+ * let loop_count = 0
+ * // 使用interval模拟频繁触发的事件
+ * const timer = setInterval(() => {
+ *     console.log('now in loop ', loop_count)
+ *     logger(loop_count)
+ *     if(loop_count ++ > 4) clearInterval(timer)
+ * }, 500)
+ *
+ * // 运行`ts-node test-debounce.ts`
+ * // 输出:
+ * now in loop  0
+ * function in debounce, now in loop  0
+ * now in loop  1
+ * now in loop  2
+ * function in debounce, now in loop  2
+ * now in loop  3
+ * now in loop  4
+ * function in debounce, now in loop  4
+ * now in loop  5
+ */
+const debounce = <Args extends Array<any>>(fn: (...args: Args) => void, min_trigger: number) => {
+    let block_timer: any = null
+    return (...args: Args) => {
+        if(block_timer === null) {
+            fn(...args)
+
+            block_timer = setTimeout(() => {
+                block_timer = null
+                clearTimeout(block_timer)
+            }, min_trigger)
+        }
+    }
+}
+
+export default debounce
