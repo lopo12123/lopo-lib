06b0cea56d5a5d720530f8cb73dd14d4d5ed3267
lopo <lopoflyfly@gmail.com>
2022-05-05 17:46:02 +0800

    feat: 节流 throttle

---
+++ lib/throttle.ts
@@ -0,0 +1,33 @@
+/**
+ * [Example & Test] {@link https://github.com/lopo12123/lopo-lib/blob/master/test/test-throttle.ts}
+ * @description [节流] 触发结束后空闲时长达到 min_trigger 后才能再次触发, 否则忽略掉本次调用
+ * @param fn 触发函数
+ * @param min_gap 最小触发间隔 (单位: ms)
+ * @example
+ * import debounce from "lopo-lib/lib/throttle";
+ *
+ * // 使用throttle包裹目标函数
+ * const logger = throttle((trigger: number) => {
+ *     console.log('function in throttle, now in trigger ', trigger)
+ * }, 1000)
+ *
+ * let trigger_count = 0
+ *
+ * // 使用interval模拟频繁调用
+ * const timer = setInterval(() => {
+ *     console.log('now in trigger ', trigger_count)
+ *     logger(trigger_count)
+ *     if(trigger_count++ > 4) clearInterval(timer)
+ * }, 500)
+ */
+const throttle = <Args extends Array<any>>(fn: (...args: Args) => void, min_gap: number) => {
+    let throttle_timer: any = Date.now()
+    return (...args: Args) => {
+        if(Date.now() - throttle_timer > min_gap) {
+            fn(...args)
+            throttle_timer = Date.now()
+        }
+    }
+}
+
+export default throttle
---
+++ test/test-throttle.ts
@@ -0,0 +1,33 @@
+import throttle from "../lib/throttle";
+
+/**
+ * @example
+ * // 运行`ts-node test-throttle.ts`
+ * // 输出:
+ * now in trigger  0
+ * now in trigger  1
+ * function in throttle, now in trigger  1
+ * now in trigger  2
+ * now in trigger  3
+ * function in throttle, now in trigger  3
+ * now in trigger  4
+ * now in trigger  5
+ * function in throttle, now in trigger  5
+ */
+const do_test = () => {
+    // 使用throttle包裹目标函数
+    const logger = throttle((trigger: number) => {
+        console.log('function in throttle, now in trigger ', trigger)
+    }, 1000)
+
+    let trigger_count = 0
+
+    // 使用interval模拟频繁调用
+    const timer = setInterval(() => {
+        console.log('now in trigger ', trigger_count)
+        logger(trigger_count)
+        if(trigger_count++ > 4) clearInterval(timer)
+    }, 500)
+}
+
+do_test()
