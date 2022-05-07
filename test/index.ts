7b6ff714cb498083af49fab3aed9b9af3054dd78
lopo <lopoflyfly@gmail.com>
2022-05-07 14:19:09 +0800

    release: v3.0.1

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const clone_1 = require("../lib/clone");
+import { clone_deep, clone_json } from "../dist/lib/clone";
+
 /**
  * @example
  * // 输出
@@ -13,14 +12,16 @@ const do_test_json = () => {
     const obj_json = {
         name: 'obj json',
         age: 11,
-        greet: (who) => {
-            return `hello ${who}!`;
+        greet: (who: string) => {
+            return `hello ${who}!`
         },
         symbol: Symbol('symbol as value'),
         [Symbol('symbol as key')]: 'symbol as key'
-    };
-    console.log(clone_1.clone_json(obj_json));
-};
+    }
+
+    console.log(clone_json(obj_json))
+}
+
 /**
  * @example
  * // 输出
@@ -38,13 +39,15 @@ const do_test_deep = () => {
         name: 'obj deep',
         age: 12,
         big: 456n,
-        greet: (who) => {
-            return `hello ${who}!`;
+        greet: (who: string) => {
+            return `hello ${who}!`
         },
         symbol: Symbol('symbol as value'),
         [Symbol('symbol as key')]: 'symbol as key'
-    };
-    console.log(clone_1.clone_deep(obj_deep));
-};
-do_test_json();
-do_test_deep();
+    }
+
+    console.log(clone_deep(obj_deep))
+}
+
+do_test_json()
+do_test_deep()
