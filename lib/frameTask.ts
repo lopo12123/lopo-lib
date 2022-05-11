55d76dc4aacc47937e5663fb8c9f9ef52de692bb
lopo <lopoflyfly@gmail.com>
2022-05-11 15:37:07 +0800

    feat: 修改了暂停逻辑, 优化了多任务暂停时的内存占用

--- lib/frameTask.ts
+++ lib/frameTask.ts
@@ -1,61 +1,93 @@
+class SingleTask<Fn extends (...args: any[]) => void = (...args: any[]) => void> {
+    // region 杂项
+    // 执行间隔
+    readonly #gap: number
+
+    // 任务名
+    readonly #taskName: string
+
+    // 回调函数
+    #fn: Fn | null
+    // endregion
+
+    // region 运行、停止、继续、取消
+    // 实例的任务封装
+    #task: ((t: number) => void) | null
+    // requestAnimationFrame返回的id, 用于取消 若为null表示当前为停止状态
+    #taskId: number | null = null
+    // 上一次调用fn的时间 若为null表示当前为停止状态
+    #last_t: number | null = null
+
+    // 当前任务是否正在运行
+    get ifRun() {
+        return this.#last_t !== null
+    }
+
+    // endregion
+
+    /**
+     * @description
+     * @param taskName 任务名, 若任务名已存在则会覆盖旧任务(旧任务停止并删除)
+     * @param fn 任务回调
+     * @param args 回调参数, 无参则传 `[]`
+     * @param gap 执行间隔
+     */
+    constructor(taskName: string, fn: Fn, args: Parameters<Fn>, gap: number) {
+        this.#taskName = taskName
+        this.#fn = fn
+        this.#gap = gap
+
+        this.#task = (t) => {
+            // 到达时间间隔则执行一次
+            if(this.#last_t === null || (t - this.#last_t) >= this.#gap) {
+                fn(...args)
+                this.#last_t = t
+            }
+
+            if(!!this.#task) this.#taskId = requestAnimationFrame(this.#task)
+        }
+    }
+
+    /**
+     * @description 运行/继续
+     */
+    run() {
+        // 当前任务存在 并且 当前为停止状态
+        if(this.#task !== null && this.#last_t === null) {
+            requestAnimationFrame(this.#task)
+        }
+    }
+
+    /**
+     * @description 暂停
+     */
+    pause() {
+        if(this.#taskId !== null && this.#last_t !== null) {
+            window.cancelAnimationFrame(this.#taskId)
+            this.#taskId = null
+            this.#last_t = null
+        }
+    }
+
+    /**
+     * @description 取消
+     */
+    cancel() {
+        if(this.#taskId !== null) {
+            window.cancelAnimationFrame(this.#taskId)
+            this.#taskId = null
+            this.#last_t = null
+            this.#task = null
+            this.#fn = null
+        }
+    }
+}
+
 class FrameTask {
-    // region fps
-    /**
-     * @see fps
-     * @private
-     */
-    #fps: number
-    /**
-     * @description how many times the task should run in 1s.
-     * @description `fps(f/s) = 1_000 / gap(ms)`
-     * @see gap
-     * @default 60
-     */
-    get fps() {
-        return this.#fps
-    }
+    readonly #fps: number
+    readonly #gap: number
 
-    set fps(val) {
-        this.#fps = val
-        this.#gap = 1000 / val
-    }
-
-    // endregion
-
-    // region gap
-    /**
-     * @see gap
-     * @private
-     */
-    #gap: number
-    /**
-     * @description gap between two run of the task.
-     * @description `gap(ms) = 1_000 / fps(f/s)`
-     * @see fps
-     * @default 16.67 (1/60 * 1000)
-     */
-    get gap() {
-        return this.#gap
-    }
-
-    set gap(val) {
-        this.#gap = val
-        this.#fps = 1000 / this.#gap
-    }
-
-    // endregion
-
-    /**
-     * @description 任务当前帧id列表
-     * @private
-     */
-    #taskIds: Map<string, number> = new Map()
-
-    /**
-     * @description 任务运行状态
-     * @private
-     */
-    #taskRun: Map<string, boolean> = new Map()
+    #tasks: Map<string, SingleTask> = new Map()
 
     /**
      * @param fps how many times the task should run in 1s.
@@ -67,168 +99,129 @@ class FrameTask {
         this.#gap = 1000 / fps
     }
 
-    /**
-     * @description 构造一个新的任务
-     * @param taskName 任务名, 若任务名已存在则会覆盖旧任务(旧任务自动取消)
-     * @param fn 任务回调
-     * @param args 回调参数, 无参则传 `[]`
-     * @private
-     */
-    private taskGenerator<Fn extends (...args: any[]) => void>(taskName: string, fn: Fn, args: Parameters<Fn>) {
-        let last_t: number | null = null
-
-        const task = (t: number) => {
-            // 暂停即不做任何操作
-            let stat = this.#taskRun.get(taskName)
-            if(!!stat) {
-                if(last_t === null) last_t = t
-
-                // 到达时间间隔则执行一次
-                if((t - last_t) >= this.#gap) {
-                    fn(...args)
-                    last_t = t
-                }
-            }
-
-            // 注册下一帧事件
-            const taskId = requestAnimationFrame(task)
-            // 更新循环 id
-            this.#taskIds.set(taskName, taskId)
-        }
-
-        return task
-    }
-
-    // region 新增、运行、暂停、删除
+    // region 新增 运行 停止 删除
     /**
      * @description 新增任务
-     * @param taskName 任务名, 若任务名已存在则会覆盖旧任务(旧任务自动取消)
+     * @param taskName 任务名, 若任务名已存在则会覆盖旧任务(旧任务停止并删除)
      * @param fn 任务回调
      * @param args 回调参数, 无参则传 `[]`
      * @param immediate 是否立即开始执行, 默认为`false`
      */
     addTask<Task extends (...args: any[]) => void>(taskName: string, fn: Task, args: Parameters<Task>, immediate: boolean = false) {
         // 如果已经注册过taskName, 覆盖并提示
-        if(this.#taskIds.has(taskName)) {
-            window.cancelAnimationFrame(this.#taskIds.get(taskName)!)
-            console.warn(`A task named [${ taskName }] has already been registered, and the old task is overwritten.`)
+        if(this.#tasks.has(taskName)) {
+            this.#tasks.get(taskName)?.cancel()
+            console.warn(`A task named [${ taskName }] has already been registered, the old task will be overwritten.`)
         }
 
-        this.#taskRun.set(taskName, immediate)
+        const newTask = new SingleTask(taskName, fn, args, this.#gap)
+        this.#tasks.set(taskName, newTask)
 
-        const task = this.taskGenerator(taskName, fn, args)
-
-        requestAnimationFrame(task)
+        if(immediate) newTask.run()
     }
 
     /**
-     * @description 开始/继续 执行任务
-     * @param taskName 任务名
+     * @description 开始/继续执行任务
      */
     runTask(taskName: string) {
-        if(!this.#taskRun.has(taskName)) {
-            console.warn(`No task named ${ taskName } found, nothing will be executed.`)
-        }
-        else this.#taskRun.set(taskName, true)
+        const task = this.#tasks.get(taskName)
+
+        if(task === undefined) console.warn(`no task named [${ taskName }].`)
+        else if(task.ifRun) console.warn(`the task named [${ taskName }] has already been running.`)
+        else task.run()
     }
 
     /**
-     * @description 暂停任务
-     * @param taskName
+     * @description 停止任务
      */
     pauseTask(taskName: string) {
-        if(!this.#taskRun.has(taskName)) {
-            console.warn(`No task named ${ taskName } found, nothing will be executed.`)
-        }
-        else this.#taskRun.set(taskName, false)
+        const task = this.#tasks.get(taskName)
+
+        if(task === undefined) console.warn(`no task named [${ taskName }].`)
+        else if(!task.ifRun) console.warn(`the task named [${ taskName }] has already paused.`)
+        else task.pause()
     }
 
     /**
-     * @description 停止并删除指定任务
-     * @see cancelExcept
-     * @see cancelAll
-     * @param taskName 任务名
+     * @description 删除单个任务
+     * @see cancelTask_with_expect
+     * @see cancelTask_all
+     * @param taskName 要删除的任务名
      */
-    cancelTask(taskName: string) {
-        if(!this.#taskRun.has(taskName)) {
-            console.warn(`No task named ${ taskName } found, nothing will be executed.`)
-        }
+    cancelTask_single(taskName: string) {
+        const task = this.#tasks.get(taskName)
+
+        if(task === undefined) console.warn(`no task named [${ taskName }].`)
         else {
-            this.#taskRun.set(taskName, false)
-            window.cancelAnimationFrame(this.#taskIds.get(taskName)!)
-            this.#taskRun.delete(taskName)
-            this.#taskIds.delete(taskName)
+            task.cancel()
+            this.#tasks.delete(taskName)
         }
     }
 
     /**
-     * @description 停止并删除(除指定任务外的)所有任务
-     * @see cancelTask
-     * @see cancelAll
-     * @param except 排除在外的任务
+     * @description 删除指定任务之外的全部任务
+     * @see cancelTask_single
+     * @see cancelTask_all
+     * @param expect 要保留的任务名列表
      */
-    cancelExcept(except: string[] = []) {
-        this.#taskRun.forEach((state, taskName) => {
-            if(!except.includes(taskName)) {
-                this.#taskRun.set(taskName, false)
-                this.#taskRun.delete(taskName)
-            }
-        })
-        this.#taskIds.forEach((taskId, taskName) => {
-            if(!except.includes(taskName)) {
-                window.cancelAnimationFrame(taskId)
-                this.#taskIds.delete(taskName)
+    cancelTask_with_expect(expect: string[]) {
+        this.#tasks.forEach((task, taskName) => {
+            if(!expect.includes(taskName)) {
+                task.cancel()
+                this.#tasks.delete(taskName)
             }
         })
     }
 
     /**
-     * @description 停止并删除全部任务
-     * @see cancelTask
-     * @see cancelExcept
+     * @description 删除全部任务
+     * @see cancelTask_single
+     * @see cancelTask_with_expect
      */
-    cancelAll() {
-        this.#taskRun.forEach((state, taskName) => {
-            this.#taskRun.set(taskName, false)
+    cancelTask_all() {
+        this.#tasks.forEach((task) => {
+            task.cancel()
         })
-        this.#taskIds.forEach((taskId) => {
-            window.cancelAnimationFrame(taskId)
-        })
-        this.#taskRun.clear()
-        this.#taskIds.clear()
+        this.#tasks.clear()
     }
+
     // endregion
 
     // region 获取状态
     /**
-     * @description 获取当前的全部任务名
-     * @see getTaskStates
+     * @description 获取当前所有任务名
      */
-    getTaskNames() {
-        const taskNames: string[] = []
-        this.#taskIds.forEach((id, taskName) => {
-            taskNames.push(taskName)
+    getState_name_list() {
+        const task_name_list: string[] = []
+        this.#tasks.forEach((task, taskName) => {
+            task_name_list.push(taskName)
         })
-        return taskNames
+        return task_name_list
     }
 
     /**
-     * @description 获取当前的全部任务状态
-     * @see getTaskNames
+     * @description 获得指定任务的运行状态
+     * @see getState_run_list
+     * @return `null` 无此任务; `true` 正在运行; `false` 停止
      */
-    getTaskStates() {
-        const taskNames: { taskName: string, ifRun: boolean }[] = []
-        this.#taskRun.forEach((ifRun, taskName) => {
-            taskNames.push({ taskName, ifRun })
-        })
-        return taskNames
+    getState_run(taskName: string) {
+        if(!this.#tasks.has(taskName)) return null
+        else return this.#tasks.get(taskName)!.ifRun
     }
+
+    /**
+     * @description 获取当前所有任务运行状态
+     * @see getState_run
+     */
+    getState_run_list() {
+        const task_name_list: { taskName: string, ifRun: boolean }[] = []
+        this.#tasks.forEach((task, taskName) => {
+            task_name_list.push({ taskName, ifRun: task.ifRun })
+        })
+        return task_name_list
+    }
+
     // endregion
 }
 
 export default FrameTask
-
-/**
- * @description
- * 逐帧任务管理. 按照某一帧率执行回调, 支持多任务、添加、暂停、取消等.
- */
