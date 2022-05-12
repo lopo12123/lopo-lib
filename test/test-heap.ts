43e116c8875f309aa3ff74bee2d6fa0dcf9e516c
lopo <lopoflyfly@gmail.com>
2022-05-12 16:52:17 +0800

    test: 堆 heap

---
+++ test/test-heap.ts
@@ -0,0 +1,29 @@
+import { Heap_big_root, Heap_small_root } from "../lib/heap";
+
+const do_test_big = () => {
+    const big_root = new Heap_big_root([ 2, 5, 1, 3, 6, 1, 2 ])
+    big_root.add(5)
+    console.log(big_root.heap)
+    big_root.delete()
+    console.log(big_root.heap)
+    big_root.delete()
+    console.log(big_root.heap)
+}
+do_test_big()
+// [ 6, 5, 3, 5, 2, 1, 1, 2 ]
+// [ 5, 5, 3, 2, 2, 1, 1 ]
+// [ 5, 2, 3, 2, 1, 1 ]
+
+const do_test_small = () => {
+    const small_root = new Heap_small_root([ 2, 5, 1, 3, 6, 1, 2 ])
+    small_root.add(5)
+    console.log(small_root.heap)
+    small_root.delete()
+    console.log(small_root.heap)
+    small_root.delete()
+    console.log(small_root.heap)
+}
+do_test_small()
+// [ 1, 1, 2, 2, 3, 5, 6, 5 ]
+// [ 1, 2, 2, 5, 3, 5, 6 ]
+// [ 2, 2, 6, 5, 3, 5 ]
