2b78e27871c3d49189c223f9bc791b0763f28e37
lopo <lopo@zju.edu.cn>
2022-06-12 23:51:37 +0800

    style: 菜单栏hover颜色 淡入淡出

---
+++ homepage/src/layouts/MenuPanel.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="menu-panel">
+        <div class="menu-title">123</div>
+        <div class="menu-item">1</div>
+        <div class="menu-item">2</div>
+        <div class="menu-item">3</div>
+        <div class="menu-item">3</div>
+        <div class="menu-item">3</div>
+        <div class="menu-item">3</div>
+        <div class="menu-item">3</div>
+        <div class="menu-item">3</div>
+        <div class="menu-item">3</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "src/styles/mixin";
+
+.menu-panel {
+    @include mixin.useScrollbar();
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden auto;
+    box-sizing: content-box;
+
+    .menu-title {
+        position: sticky;
+        top: 0;
+        z-index: 10;
+        width: 100%;
+        height: 40px;
+        background-color: #e4e7ed;
+    }
+
+    .menu-item {
+        width: 100%;
+        height: 40px;
+        padding: 0 20px;
+        cursor: pointer;
+        user-select: none;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        transition: background-color 1s ease-out;
+        &:hover{
+            background-color: #83e5cf80;
+        }
+    }
+}
+</style>
--- homepage/src/views/Home.vue
+++ homepage/src/views/Home.vue
@@ -1,11 +1,19 @@
 <script lang="ts" setup>
+import Splitter from "primevue/splitter";
+import SplitterPanel from "primevue/splitterpanel";
+import MenuPanel from "@/layouts/MenuPanel.vue";
 
 </script>
 
 <template>
-    <div class="home">
-        home page
-    </div>
+    <Splitter class="home">
+        <SplitterPanel class="menu-container" :size="20" :min-size="20">
+            <MenuPanel/>
+        </SplitterPanel>
+        <SplitterPanel class="doc-container" :size="80" :min-size="60">
+            doc
+        </SplitterPanel>
+    </Splitter>
 </template>
 
 <style lang="scss" scoped>
@@ -13,5 +21,13 @@
     position: relative;
     width: 100%;
     height: 100%;
+
+    .menu-container {
+
+    }
+
+    .doc-container {
+
+    }
 }
 </style>
