fd304f3c86234ac58cd4e39c6053e54204f0b3ea
lopo <lopo@zju.edu.cn>
2022-06-12 23:32:22 +0800

    chore(deps): primevue

--- homepage/src/main.tsx
+++ homepage/src/main.tsx
@@ -1,5 +1,13 @@
+// style
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "@/styles/index.scss";
+
+// core
 import { createApp } from "vue";
 
+// plugin
 import { router } from "@/routers";
 
 createApp(() => <router-view/>)
